653b8d1024697de1a7edf570250393e4ad254585
Ricardo Martín del campo Loza <ricer.loza@gmail.com>
2025-02-21 21:29:16 -0600

    First complete version, add result view, add possibility to play again, add responsive styles

--- script.js
+++ script.js
@@ -3,20 +3,55 @@ import { wordList } from './wordsList.js';
 const container = document.querySelector('.container');
 const timer = document.querySelector('.timer');
 const appContainer = document.querySelector('.app-container');
+const gameContainer = document.querySelector('.game-container');
+const resultsContainer = document.querySelector('.results-container');
+const resultWPM = document.querySelector('.result-wpm');
+const resultAcc = document.querySelector('.result-acc');
+const btnRestart = document.querySelector('.btn-restart');
+
+appContainer.addEventListener('keydown', checkCorrectKeyPressed);
+btnRestart.addEventListener('click', () => {
+  initGame();
+  startCaret();
+});
 
 function startTimer() {
   let timerCount = 60;
   const timerInterval = setInterval(() => {
     timer.textContent = timerCount;
     timerCount--;
-    if (timerCount === 0) clearInterval(timerInterval);
+    if (timerCount === 0) {
+      getResultView();
+      resultsContainer.style.display = 'flex';
+      gameContainer.style.display = 'none';
+      clearInterval(timerInterval);
+    }
   }, 1000);
+}
+
+function getResultView() {
+  const totalWords = gameContainer.querySelectorAll('.word');
+
+  const { correctChars, wrongChars, completedWords } = [...totalWords].reduce(
+    (acc, word) => {
+      const correctChars = word.querySelectorAll('.correct');
+      const wrongChars = word.querySelectorAll('.wrong');
+      if (correctChars.length + wrongChars.length === word.childElementCount) acc.completedWords.push(word);
+      if (correctChars.length) acc.correctChars.push([...correctChars]);
+      if (wrongChars.length) acc.wrongChars.push([...correctChars]);
+      return acc;
+    },
+    { correctChars: [], wrongChars: [], completedWords: [] },
+  );
+  const correctWordsPct = (correctChars.length * 100) / (correctChars.length + wrongChars.length);
+  resultWPM.textContent = completedWords.length;
+  resultAcc.textContent = correctWordsPct ? `${Math.floor(correctWordsPct)}%` : '0%';
 }
 
 function getRandomWordList(wordList) {
   let randomWordList = [];
   let randomWord = '';
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 100; i++) {
     randomWord = wordList[Math.floor(Math.random() * wordList.length - 1) + 1];
     randomWordList.push(randomWord);
   }
@@ -95,14 +130,13 @@ function checkCorrectKeyPressed(event) {
 }
 
 function initGame() {
+  gameContainer.style.display = 'flex';
+  resultsContainer.style.display = 'none';
+  appContainer.focus();
   const randomWordList = getRandomWordList(wordList);
   generateWordElementString(randomWordList);
   startCaret();
 
-  // startTimer();
-
-  appContainer.addEventListener('keydown', checkCorrectKeyPressed);
+  startTimer();
 }
-
 initGame();
-
